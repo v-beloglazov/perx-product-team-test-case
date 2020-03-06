c898a5352c47c84dd69eefe4590d7a85ffd2d710
Vyacheslav Beloglazov <beloglazov@quancy.com.sg>
2020-03-06 16:47:01 +0500

    HMR

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
-const render = initialData => {
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./reducers', () => {
+    const newRootReducer = require('./reducers').default
+    store.replaceReducer(newRootReducer)
+  })
+}
+
+const render = (initialData = {}) => {
   const App = require('./App').default;
 
   const { dealers } = initialData;
@@ -22,8 +29,14 @@ const render = initialData => {
     document.getElementById('root')
   );
 };
-const initApp = (initialData = {}) => render(initialData);
+
+const initApp = initialData => render(initialData);
+
 window.initReactApp = initApp;
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
